494c5b6d27c1eccf4e5f2f0bfd697ffe7519e786
Akanuma Hiroaki <hiroaki.akanuma@gmail.com>
2017-05-24 23:15:58 +0000

    Use HTTPClient and Logger.

--- room_notify.rb
+++ room_notify.rb
@@ -1,15 +1,18 @@
 require 'bundler/setup'
-require 'net/http'
+require 'httpclient'
+require 'logger'
 require 'pi_piper'
 require './distance.rb'
+
+BEAM_URL = 'http://beam.soracom.io:8888/room'
 
 TRIG_GPIO = 17
 ECHO_GPIO = 27
 LED_GPIO  = 18
 THRESHOLD = 40 # cm
 
-trig_pin = PiPiper::Pin.new(pin: TRIG_GPIO, direction: :out)
-echo_pin = PiPiper::Pin.new(pin: ECHO_GPIO, direction: :in)
+logger = Logger.new('room_notify.log')
+
 led_pin  = PiPiper::Pin.new(pin: LED_GPIO,  direction: :out)
 
 occupied = false
@@ -20,36 +23,26 @@ unless ARGV.empty?
   interval = ARGV.first.to_f
 end
 
+http_client = HTTPClient.new
 loop do
   start_time = Time.now
-  distance = read_distance(trig_pin, echo_pin)
+  distance = read_distance(TRIG_GPIO, ECHO_GPIO)
 
   current_status = distance < THRESHOLD
   if occupied != current_status
     duration = Time.now - state_changed_at
     state_changed_at = Time.now
     message = "Distance: %.1f cm - Status changed to %s. (Duration: %d sec)" % [distance, current_status ? 'OCCUPIED' : 'EMPTY', duration]
-    puts message
     payload = '{"text":"%s"}' % message
-    uri = URI.parse('http://beam.soracom.io:8888/room')
-    http = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Post.new(uri.path)
-    req.body = payload
-    res = http.start {|http| http.request(req) }
-    puts "Beam Response: #{res.code}"
+    res = http_client.post(BEAM_URL, payload, 'Content-Type' => 'application/json')
+    logger.info("PAYLOAD: #{payload} / BEAM Response: #{res.status}")
     occupied = current_status
   end
 
-  if occupied
-    led_pin.on
-  else
-    led_pin.off
-  end
+  occupied ? led_pin.on : led_pin.off
 
   if Time.now < start_time + interval
     sleep(start_time + interval - Time.now)
     next
   end
-
-  sleep(0.1)
 end
