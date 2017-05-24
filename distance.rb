1e726d292a8caf9981da1dadec776c2f2a6fe5df
Akanuma Hiroaki <hiroaki.akanuma@gmail.com>
2017-05-24 23:14:58 +0000

    Use edge detection.

--- distance.rb
+++ distance.rb
@@ -4,7 +4,9 @@ require 'pi_piper'
 TRIG_GPIO = 17
 ECHO_GPIO = 27
 
-def read_distance(trig_pin, echo_pin)
+def read_distance(trig_pin_no, echo_pin_no)
+  trig_pin = PiPiper::Pin.new(pin: trig_pin_no, direction: :out)
+  echo_pin = PiPiper::Pin.new(pin: echo_pin_no, direction: :in, trigger: :both)
   trig_pin.off
   sleep(0.3)
 
@@ -12,37 +14,31 @@ def read_distance(trig_pin, echo_pin)
   sleep(0.00001)
   trig_pin.off
 
+  echo_pin.wait_for_change
   signal_off = Time.now
-  echo_pin.read
-  while echo_pin.off?
-    signal_off = Time.now
-    echo_pin.read
-  end
 
   signal_on = signal_off
-  while echo_pin.on?
-    echo_pin.read
-  end
+  echo_pin.wait_for_change
   signal_on = Time.now
 
   time_passed = signal_on - signal_off
   distance = time_passed * 17_000
 
+  PiPiper::Platform.driver.unexport_pin(trig_pin_no)
+  PiPiper::Platform.driver.unexport_pin(echo_pin_no)
+
   return distance if distance <= 500
 end
 
 if $0 == __FILE__
-  trig_pin = PiPiper::Pin.new(pin: TRIG_GPIO, direction: :out)
-  echo_pin = PiPiper::Pin.new(pin: ECHO_GPIO, direction: :in)
-
   loop do
     start_time = Time.now
-    distance = read_distance(trig_pin, echo_pin)
+    distance = read_distance(TRIG_GPIO, ECHO_GPIO)
     unless distance.nil?
       puts "Distance: %.1f cm" % distance
     end
 
-    wait = start_time + 2 - Time.now
+    wait = start_time + 3 - Time.now
     sleep(wait) if wait > 0
   end
 end
